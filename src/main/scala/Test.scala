cd07b2bc65d63510a8e78a4141c4c0a707a4e075
Jason Zaugg <jzaugg@gmail.com>
2018-11-12 13:38:50 +1000

    Rework with packSymbols, existentialTransform

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,7 +1,15 @@
-import scala.tools.nsc._
 import scala.collection.mutable.ListBuffer
+import scala.tools.nsc._
 
 class FBound[A <: FBound[A]]
+
+class Bounded[A <: String, B <: A]
+
+class Entity[A] {
+  class Inner
+
+  type C = Option[Inner]
+}
 
 object ScratchMain {
   def main(args: Array[String]): Unit = {
@@ -14,21 +22,97 @@ class Scratch {
   import global._
 
   global.settings.usejavacp.value = true
-  global.settings.debug.value = true
   new Run
 
   val infer = analyzer.newTyper(analyzer.NoContext).infer
+  def assertEquals(expected: String, tp: Type): Unit = {
+    if (expected != tp.toString()) throw new AssertionError(s"Expected $expected, found $tp")
+  }
 
   def test(): Unit = {
     exitingTyper {
-      val symbol = symbolOf[FBound[_]]
-      symbol.initialize
-      val input = symbol.tpe_*
-      println(input)
-      val result = new AbstractToBoundsMap(symbol.asClass).apply(input)
-      checkAllBounds(result)
+      assertEquals("FBound[FBound[_0]] forSome { type _0 }", packAndApproximate(symbolOf[FBound[_]].initialize.tpe_*, symbolOf[FBound[_]]))
+      assertEquals("Bounded[String,String]", packAndApproximate(symbolOf[Bounded[_, _]].initialize.tpe_*, symbolOf[Bounded[_, _]]))
+      assertEquals("Option[Entity[A]#Inner]", packAndApproximate(typeOf[Entity[Any]].decl(TypeName("C")).info, symbolOf[Entity[_]]))
     }
     assert(!reporter.hasErrors)
+  }
+
+  /**
+    * Compute an existential type from hidden symbols that have `owner` as a transtive owner and type `tp`.
+    * @param ownerOfHidden    The symbol enclosing the symbols that must be existentially abstracted
+    * @param tp               The original type
+    */
+
+  def pack(tp: Type, ownerOfHidden: Symbol) = {
+    var hidden: List[Symbol] = Nil
+    val seen = collection.mutable.Set[Symbol]()
+    for (t <- tp) {
+      val bts = t.baseTypeSeq.toList
+      for (b <- bts) {
+        for (t <- b) {
+          val sym = t.typeSymbolDirect
+          if (sym.hasTransOwner(ownerOfHidden) && sym != ownerOfHidden) {
+            if (!seen(sym)) {
+              seen += sym
+              hidden ::= sym
+            }
+          }
+        }
+      }
+    }
+    packSymbols(hidden, tp)
+  }
+
+  def packAndApproximate(tp: Type, hiddenOwner: Symbol): Type = {
+    val packed = pack(tp, hiddenOwner)
+    val newExistentials = collection.mutable.LinkedHashMap[Symbol, Symbol]()
+    var eid: Int = 0
+    def newExistentialFor(sym: Symbol): Symbol ={
+      newExistentials.get(sym) match {
+        case Some(esym) => esym
+        case _ =>
+          val existential = NoSymbol.newExistential("_" + eid).setInfo(TypeBounds.empty)
+          eid += 1
+          newExistentials(sym) = existential
+          existential
+      }
+    }
+    // Approximate existentials by the variance-appropriate bound. F-Bounded existentials are unpeeled
+    // once before resorting to a fresh, unbounded existential argument.
+    val approximateExistentials: TypeMap = new TypeMap(trackVariance = true) {
+      var boundStack: List[Symbol] = Nil
+      override def apply(tp: Type): Type = tp match {
+        case TypeRef(ThisType(parentSym), sym, args) if parentSym.hasTransOwner(hiddenOwner) =>
+          // Convert `Hidden.this.Inner` to `Hidden#Inner`
+          apply(TypeRef(parentSym.tpe, sym, args))
+        case tr: TypeRef if tr.sym.isExistential =>
+          if (boundStack.contains(tr.sym)) {
+            TypeRef(NoPrefix, newExistentialFor(tr.sym), Nil)
+          } else {
+            boundStack ::= tr.sym
+            try {
+              val bound = if (variance.isContravariant) tp.bounds.lo else tp.bounds.hi
+              mapOver(bound)
+            } finally
+              boundStack = boundStack.tail
+          }
+        case _ => mapOver(tp)
+      }
+    }
+    val approximated = approximateExistentials.apply(packed)
+    // Wrap the approximated result in an ExistentialType if fresh quantified symbols have been created.
+    // Note that the `newExistentialType` constructor collapses `ExistentialType(ExistentialType(...)`.
+    val wrapped = newExistentialType(newExistentials.valuesIterator.toList, approximated)
+    val result = wrapped match {
+      case et: ExistentialType =>
+        // Clean up the resulting type by removing existential quantifers that are not referenced in the underlying
+        // type.
+        existentialAbstraction(et.quantified, et.underlying)
+      case tp => tp
+    }
+    checkAllBounds(result)
+    result
   }
 
   // START COPY/PASTE from refchecks
@@ -49,89 +133,13 @@ class Scratch {
 
   private def checkTypeRef(tp: Type, tree: Tree, skipBounds: Boolean) = tp match {
     case TypeRef(pre, sym, args) =>
-      if(sym.isJavaDefined)
+      if (sym.isJavaDefined)
         sym.typeParams foreach (_.cookJavaRawInfo())
       if (!tp.isHigherKinded && !skipBounds)
         infer.checkBounds(tree, pre, sym.owner, sym.typeParams, args, "")
     case _ =>
   }
+
   // END COPY/PASTE from refchecks
 
-
-  private class AbstractToBoundsMap(eeSym: ClassSymbol) extends TypeMap(trackVariance = true) {
-    import scala.collection.mutable
-
-    // the (prefix, type param symbol) that correspond to encountered existential quantifiers
-    private val existentialTparams = mutable.HashMap[Symbol, (Type, Symbol)]()
-    // prevent infinite recursion over recursive types
-    private val seen = mutable.HashMap[Symbol, Type]()
-
-    private val newExistentials = mutable.HashMap[Symbol, Symbol]()
-
-    override def apply(tp: Type): Type = {
-      val newType = tp match {
-        case tr@TypeRef(pre, sym, args) =>
-          foreach2(sym.typeParams, args) { (tparam, arg) =>
-            if (arg.typeSymbolDirect.isExistential)
-              existentialTparams(arg.typeSymbolDirect) = (pre, tparam)
-          }
-
-          if (sym.isAbstractType || sym.isExistential) {
-            seen.get(sym) match {
-              case None =>
-                // we've not seen this symbol before
-                seen(sym) = NoType
-
-                def boundOf(tp: Type): Type = if (variance.isContravariant) tp.bounds.lo else tp.bounds.hi
-
-                val bound = if (sym.isExistential && existentialTparams.contains(sym)) {
-                  val (pre, tparam) = existentialTparams(sym) // TODO probably limit this to the case when tparam.info.bounds = (Nothing, Any)
-                  boundOf(tparam.info).asSeenFrom(pre, tparam.owner)
-                } else boundOf(tp)
-
-                val ret =
-                  if (bound ne tp) apply(bound) // recurse to unpeel layers of abstract types
-                  else mapOver(project(bound))
-
-                // memoise the result of this type transformation
-                seen(sym) = ret
-                ret
-              case Some(NoType) =>
-                // we're in the middle of recursing over this type, so short-circuit to avoid infinite recursion
-                newExistentials
-                  .getOrElseUpdate(sym, {
-                    sym.owner.freshExistential("", newExistentials.size).setInfo(TypeBounds.empty)
-                  })
-                  .tpe
-              case Some(t) =>
-                // we've seen (and fully resolved) this type before, so just return the memoised result
-                t
-            }
-
-          } else mapOver(tr)
-        case t =>
-          mapOver(t)
-      }
-
-      // if we've short circuited lower down, then we need to wrap all the way back up with ExistentialType
-      if (newExistentials.nonEmpty && !newType.typeSymbol.isExistential) {
-        newType match {
-          case ExistentialType(quantified, underlying) =>
-            ExistentialType((quantified ::: newExistentials.values.toList).distinct, underlying)
-          case t =>
-            ExistentialType(newExistentials.values.toList, t)
-        }
-      } else {
-        newType
-      }
-    }
-
-    // Convert MyEntity.this.Inner to MyEntity#Inner
-    private def project(tp: Type) = tp match {
-      case TypeRef(ThisType(parentSym), sym, args) if parentSym.ownerChain.contains(eeSym) =>
-        TypeRef(parentSym.tpe, sym, args)
-      case _ =>
-        tp
-    }
-  }
 }
