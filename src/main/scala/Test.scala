619db249aef1f6cb17e2002a1657155bb67b66bb
Jason Zaugg <jzaugg@gmail.com>
2018-11-14 09:47:03 +1000

    Rework without packSymbols.

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -5,7 +5,13 @@ class FBound[A <: FBound[A]]
 
 class Bounded[A <: String, B <: A]
 
+class Bounded1[A <: Bounded[_, _]]
+
+trait Foo1[M <: String, P <: Foo1[M, _]]
+trait Foo2[M <: String, P <: Foo2[M, P]]
+
 class Entity[A] {
+
   class Inner
 
   type C = Option[Inner]
@@ -25,56 +31,37 @@ class Scratch {
   new Run
 
   val infer = analyzer.newTyper(analyzer.NoContext).infer
-  def assertEquals(expected: String, tp: Type): Unit = {
-    if (expected != tp.toString()) throw new AssertionError(s"Expected $expected, found $tp")
+  def check(expected: String, tp: Type): Unit = {
+    val approximated = approximate(tp)
+    if (expected != approximated.toString())
+      throw new AssertionError(s"Expected $expected, found $approximated")
+    // Perform the same bounds checking as refchecks
+    checkAllBounds(approximated)
   }
 
   def test(): Unit = {
     exitingTyper {
-      assertEquals("FBound[FBound[_0]] forSome { type _0 }", packAndApproximate(symbolOf[FBound[_]].initialize.tpe_*, symbolOf[FBound[_]]))
-      assertEquals("Bounded[String,String]", packAndApproximate(symbolOf[Bounded[_, _]].initialize.tpe_*, symbolOf[Bounded[_, _]]))
-      // TODO type arguments [Any,Any] do not conform to class Bounded's type parameter bounds [A <: String,B <: A]
-      //      We'd like `Bounded[String, String]
-      // assertEquals("Bounded[String,String]", packAndApproximate(typeOf[Bounded[_, _]], symbolOf[Bounded[_, _]]))
-      assertEquals("Option[Entity[A]#Inner]", packAndApproximate(typeOf[Entity[Any]].decl(TypeName("C")).info, symbolOf[Entity[_]]))
+      check("Bounded[String,String]", typeOf[Bounded[_, _]])
+      check("Bounded1[Bounded[String,String]]", typeOf[Bounded1[_]])
+      check("FBound[FBound[_0]] forSome { type _0 }", symbolOf[FBound[_]].initialize.tpe_*)
+      check("Foo1[String,Foo1[String,_0]] forSome { type _0 }", typeOf[Foo1[_, _]])
+      check("Foo2[String,Foo2[String,Foo2[String,_0]]] forSome { type _0 }", typeOf[Foo2[_, _]])
+
+
+      check("Bounded[String,String]", symbolOf[Bounded[_, _]].initialize.tpe_*)
+      check("Option[Entity[Any]#Inner]", typeOf[Entity[Any]].decl(TypeName("C")).info)
+
+      check("Foo1[String,Foo1[String,_0]] forSome { type _0 }", symbolOf[Foo1[_, _]].initialize.tpe_*)
+      check("Foo2[String,Foo2[String,_0]] forSome { type _0 }", symbolOf[Foo2[_, _]].initialize.tpe_*)
     }
     assert(!reporter.hasErrors)
   }
 
-  /**
-    * Compute an existential type from hidden symbols that have `owner` as a transtive owner and type `tp`.
-    * @param ownerOfHidden    The symbol enclosing the symbols that must be existentially abstracted
-    * @param tp               The original type
-    */
-
-  def pack(tp: Type, ownerOfHidden: Symbol) = {
-    var hidden: List[Symbol] = Nil
-    val seen = collection.mutable.Set[Symbol]()
-    for (t <- tp) {
-      val sym = tp.typeSymbolDirect
-      if (sym.hasTransOwner(ownerOfHidden)) {
-        val bts = t.baseTypeSeq.toList
-        for (b <- bts) {
-          for (t <- b) {
-            val baseTypeElementSym = t.typeSymbolDirect
-            if (baseTypeElementSym.hasTransOwner(ownerOfHidden) && baseTypeElementSym != ownerOfHidden) {
-              if (!seen(baseTypeElementSym)) {
-                seen += baseTypeElementSym
-                hidden ::= baseTypeElementSym
-              }
-            }
-          }
-        }
-      }
-    }
-    packSymbols(hidden, tp)
-  }
-
-  def packAndApproximate(tp: Type, hiddenOwner: Symbol): Type = {
-    val packed = pack(tp, hiddenOwner)
+  def approximate(tp: Type): Type = {
     val newExistentials = collection.mutable.LinkedHashMap[Symbol, Symbol]()
     var eid: Int = 0
-    def newExistentialFor(sym: Symbol): Symbol ={
+
+    def newExistentialFor(sym: Symbol): Symbol = {
       newExistentials.get(sym) match {
         case Some(esym) => esym
         case _ =>
@@ -84,41 +71,71 @@ class Scratch {
           existential
       }
     }
-    // Approximate existentials by the variance-appropriate bound. F-Bounded existentials are unpeeled
-    // once before resorting to a fresh, unbounded existential argument.
-    val approximateExistentials: TypeMap = new TypeMap(trackVariance = true) {
-      var boundStack: List[Symbol] = Nil
+
+    val map = new TypeMap(trackVariance = true) {
+
+      case class AppliedParam(pre: Type, tparam: Symbol, arg: Type, detectedFBound: Boolean)
+
+      val tparamStack = collection.mutable.ArrayStack[AppliedParam]()
+      val abstractTypeStack = collection.mutable.ArrayStack[(Type, Symbol)]()
+      val eliminatedExistentials = collection.mutable.Set[Symbol]()
+
       override def apply(tp: Type): Type = tp match {
-        case TypeRef(ThisType(parentSym), sym, args) if parentSym.hasTransOwner(hiddenOwner) =>
+        case TypeRef(ThisType(parentSym), sym, args) if !parentSym.isStaticOwner =>
           // Convert `Hidden.this.Inner` to `Hidden#Inner`
           apply(TypeRef(parentSym.tpe, sym, args))
-        case tr: TypeRef if tr.sym.isExistential =>
-          if (boundStack.contains(tr.sym)) {
-            TypeRef(NoPrefix, newExistentialFor(tr.sym), Nil)
-          } else {
-            boundStack ::= tr.sym
-            try {
-              val bound = if (variance.isContravariant) tp.bounds.lo else tp.bounds.hi
-              mapOver(bound)
-            } finally
-              boundStack = boundStack.tail
+        case tr@TypeRef(pre, sym, Nil) if sym.isExistential && tr.bounds.isEmptyBounds =>
+          tparamStack.headOption match {
+            case Some(AppliedParam(pre, tparam, `tr`, detectedFBound)) =>
+              eliminatedExistentials += sym
+              if (detectedFBound) TypeRef(NoPrefix, newExistentialFor(tparam), Nil)
+              else {
+                val bound = if (variance.isContravariant) tparam.info.bounds.lo else tparam.info.bounds.hi
+                val tp1 = bound.asSeenFrom(pre, tparam.owner)
+                if (tp1 ne tr) apply(tp1) else tp1
+              }
+            case _ =>
+              mapOver(tr)
+          }
+        case tr@TypeRef(pre, sym, Nil) if sym.isAbstractType =>
+          abstractTypeStack.find(_._2 eq sym) match {
+            case Some((pre, sym)) =>
+              TypeRef(NoPrefix, newExistentialFor(sym), Nil)
+            case None =>
+              abstractTypeStack.push((pre, sym))
+              try {
+                val bound = if (variance.isContravariant) sym.info.bounds.lo else sym.info.bounds.hi
+                val tp1 = bound.asSeenFrom(pre, sym.owner)
+                if (tp1 ne tr) apply(tp1) else tp1
+              } finally abstractTypeStack.pop()
+          }
+        case tr@TypeRef(pre, sym, args) =>
+          val pre1 = apply(pre)
+          val args1 = mapOverArgs(pre, args, sym.typeParams)
+          if ((pre1 eq pre) && (args1 eq args)) tp
+          else copyTypeRef(tp, pre1, tr.coevolveSym(pre1), args1)
+        case ExistentialType(qs, underlying) =>
+          mapOver(tp) match {
+            case ExistentialType(qs1, underlying1) if qs1.exists(eliminatedExistentials.contains) =>
+              // Clean up the resulting type by removing existential quantifers that are not referenced in the underlying type.
+              existentialAbstraction(qs1, underlying1)
+            case tp => tp
           }
         case _ => mapOver(tp)
       }
+      private def mapOverArgs(pre: Type, args: List[Type], tparams: List[Symbol]): List[Type] = {
+        map2Conserve(args, tparams) { (arg, tparam) =>
+          val detectedFBound = tparamStack.exists(_.tparam eq tparam)
+          tparamStack.push(AppliedParam(pre, tparam, arg, detectedFBound))
+          try withVariance(variance * tparam.variance)(this (arg))
+          finally tparamStack.pop()
+        }
+      }
     }
-    val approximated = approximateExistentials.apply(packed)
+    val approximated = map(tp)
     // Wrap the approximated result in an ExistentialType if fresh quantified symbols have been created.
     // Note that the `newExistentialType` constructor collapses `ExistentialType(ExistentialType(...)`.
-    val wrapped = newExistentialType(newExistentials.valuesIterator.toList, approximated)
-    val result = wrapped match {
-      case et: ExistentialType =>
-        // Clean up the resulting type by removing existential quantifers that are not referenced in the underlying
-        // type.
-        existentialAbstraction(et.quantified, et.underlying)
-      case tp => tp
-    }
-    checkAllBounds(result)
-    result
+    newExistentialType(newExistentials.valuesIterator.toList, approximated)
   }
 
   // START COPY/PASTE from refchecks
